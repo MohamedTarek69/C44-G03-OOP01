d3e00b0195f423f77c962d94b3a7d7b9c95c0014
MohamedTarek69 <mohamedyoyo960@gmail.com>
2025-07-26 17:52:57 +0300

    Q4

--- Assignment OOP 01/Program.cs
+++ Assignment OOP 01/Program.cs
@@ -7,25 +7,24 @@ namespace Assignment_OOP_01
         static void Main(string[] args)
         {
             #region Q1- Create an Enum called "WeekDays" with the days of the week (Monday to Sunday) as its members. Then, write a C# program that prints out all the days of the week using this Enum.
-            //WeekDays weekDays = new WeekDays();
             //Console.WriteLine("Days of the week:");
-            //foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)))
+            //foreach (WeekDays Day in Enum.GetValues(typeof(WeekDays)))
             //{
-            //    Console.WriteLine(day);
+            //    Console.WriteLine(Day);
             //}
 
             #endregion
 
             #region Q2- Create an Enum called "Seas on" with the four seasons (Spring, Summer, Autumn, Winter) as its members. Write a C# program that takes a season name as input from the user and displays the corresponding month range for that season. Note range for seasons ( spring march to may , summer june to august , autumn September to November , winter December to February)
-            //Seas_on season;
-            //bool isParse;
+            //Seas_on Season;
+            //bool IsParse;
             //do
             //{
             //    Console.Write("Enter Season (Spring, Summer, Autumn, Winter): ");
-            //    isParse = Enum.TryParse(Console.ReadLine(), true, out season);
-            //} while (!isParse);
+            //    IsParse = Enum.TryParse(Console.ReadLine(), true, out Season);
+            //} while (!IsParse);
 
-            //switch(season)
+            //switch (Season)
             //{
             //    case Seas_on.Spring:
             //        Console.WriteLine("Spring: March to May");
@@ -39,7 +38,7 @@ namespace Assignment_OOP_01
             //    case Seas_on.Winter:
             //        Console.WriteLine("Winter: December to February");
             //        break;
-            //    }
+            //}
 
             #endregion
 
