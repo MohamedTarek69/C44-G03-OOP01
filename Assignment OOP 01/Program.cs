68cdac2d5179a5feadff57a4edee74326ee1969e
MohamedTarek69 <mohamedyoyo960@gmail.com>
2025-07-26 16:12:38 +0300

    Q2

---
+++ Assignment OOP 01/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Assignment_OOP_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1- Create an Enum called "WeekDays" with the days of the week (Monday to Sunday) as its members. Then, write a C# program that prints out all the days of the week using this Enum.
+            //WeekDays weekDays = new WeekDays();
+            //Console.WriteLine("Days of the week:");
+            //foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)))
+            //{
+            //    Console.WriteLine(day);
+            //}
+
+            #endregion
+
+            #region Q2- Create an Enum called "Seas on" with the four seasons (Spring, Summer, Autumn, Winter) as its members. Write a C# program that takes a season name as input from the user and displays the corresponding month range for that season. Note range for seasons ( spring march to may , summer june to august , autumn September to November , winter December to February)
+            //Seas_on season;
+            //bool isParse;
+            //do
+            //{
+            //    Console.Write("Enter Season (Spring, Summer, Autumn, Winter): ");
+            //    isParse = Enum.TryParse(Console.ReadLine(), true, out season);
+            //} while (!isParse);
+
+            //switch(season)
+            //{
+            //    case Seas_on.Spring:
+            //        Console.WriteLine("Spring: March to May");
+            //        break;
+            //    case Seas_on.Summer:
+            //        Console.WriteLine("Summer: June to August");
+            //        break;
+            //    case Seas_on.Autumn:
+            //        Console.WriteLine("Autumn: September to November");
+            //        break;
+            //    case Seas_on.Winter:
+            //        Console.WriteLine("Winter: December to February");
+            //        break;
+            //    }
+
+            #endregion
+
+            }
+    }
+}
